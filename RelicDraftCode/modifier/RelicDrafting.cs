3b1b4b1a38decfd9511b6e37822765f8cf5a574e
Sean Beach <dirtybeach0728@gmail.com>
2026-06-07 15:32:11 -0500

    re-add modifier class

---
+++ RelicDraftCode/modifier/RelicDrafting.cs
@@ -0,0 +1,8 @@
+using BaseLib.Abstracts;
+
+namespace RelicDraft.RelicDraftCode.modifier;
+
+public class RelicDrafting : CustomModifierModel
+{
+    public override ModifierAlignment Alignment => ModifierAlignment.Good;
+}

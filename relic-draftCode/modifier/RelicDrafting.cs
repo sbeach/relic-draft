a02b3d36127a7a5d7bb0721ef22a613d73a3bc1a
Sean Beach <dirtybeach0728@gmail.com>
2026-06-07 14:28:11 -0500

    impl ModifierModel

--- relic-draftCode/modifier/RelicDrafting.cs
+++ relic-draftCode/modifier/RelicDrafting.cs
@@ -2,7 +2,7 @@ using BaseLib.Abstracts;
 
 namespace relic_draft.relic_draftCode.modifier;
 
-public class RelicDrafting
+public class RelicDrafting : CustomModifierModel
 {
-    
+    public override ModifierAlignment Alignment { get; }
 }

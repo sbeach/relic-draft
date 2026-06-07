bb419230c60b6d665687b3e1c18c46d4b13351e4
Sean Beach <dirtybeach0728@gmail.com>
2026-06-07 16:29:08 -0500

    define title and description for modifier

--- RelicDraftCode/modifier/RelicDrafting.cs
+++ RelicDraftCode/modifier/RelicDrafting.cs
@@ -1,9 +1,14 @@
 using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Localization;
 
 namespace RelicDraft.RelicDraftCode.modifier;
 
 public class RelicDrafting : CustomModifierModel
 {
+    public override LocString Title => new("modifiers", Id.Entry + ".title");
+
+    public override LocString Description => new("modifiers", Id.Entry + ".description");
+
     public override ModifierAlignment Alignment => ModifierAlignment.Good;
 
     // negative value makes our mod show up at the top of the modifiers list for easier access

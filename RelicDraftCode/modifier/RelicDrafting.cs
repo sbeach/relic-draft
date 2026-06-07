900ff42daf603c696ef1512811f8657b7e66cbf3
Sean Beach <dirtybeach0728@gmail.com>
2026-06-07 15:36:49 -0500

    add comment

--- RelicDraftCode/modifier/RelicDrafting.cs
+++ RelicDraftCode/modifier/RelicDrafting.cs
@@ -6,5 +6,6 @@ public class RelicDrafting : CustomModifierModel
 {
     public override ModifierAlignment Alignment => ModifierAlignment.Good;
 
+    // negative value makes our mod show up at the top of the modifiers list for easier access
     public override int SortOrder => -1;
 }
